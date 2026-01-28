b554cdc7b2a370e92788fd36803d9885bbbe69e9
Christopher Lewis <christopher.lewis@thecloudxpert.net>
2026-01-28 19:56:26 +0000

    Config updates

--- labfiles/hol-2601-50/web.sh
+++ labfiles/hol-2601-50/web.sh
@@ -3,7 +3,7 @@ sudo mkdir -p /var/www/${SITE}/public
 echo "<html><body><h1>Welcome to the HOL 2601-50 Web Server</h1></body></html>" | sudo tee /var/www/${SITE}/public/index.html > /dev/null
 sudo chown -R www-data:www-data /var/www/${SITE}
 sudo chmod -R 755 /var/www/${SITE}
-sudo bash tee > /etc/apache2/sites-available/${SITE}.conf << EOF
+cat <<EOF | sudo tee /etc/apache2/sites-available/${SITE}.conf
 <VirtualHost *:80>
     ServerName $SITE
     DocumentRoot /var/www/${SITE}/public
