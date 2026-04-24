e11a3bd7df7d3e2711836acdd0c0ec8a7f7e860f
Christopher Lewis <christopher-d.lewis@broadcom.com>
2026-04-25 00:58:28 +0100

    updated to add missing "

--- gitlab.sh
+++ gitlab.sh
@@ -7,7 +7,7 @@ TOKEN=$(</home/holuser/gitlab.txt)
 GITLAB_REPO_DIR="${SCRIPT_DIR}/gitlab"
 BRANCH="main"
 USERNAME="hol.admin"
-REMOTE_URL="https://${USERNAME}:${TOKEN}@gitlab.site-a.vcf.lab/hol.admin/hol-all-apps.git
+REMOTE_URL="https://${USERNAME}:${TOKEN}@gitlab.site-a.vcf.lab/hol.admin/hol-all-apps.git"
 
 if [ ! -d "$GITLAB_REPO_DIR" ]; then
     echo "[INFO] Gitlab folder missing."
