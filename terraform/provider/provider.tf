b5561f3696626586b9b20391c14096c27b9cfc4c
Christopher Lewis <christopher-d.lewis@broadcom.com>
2026-05-08 02:16:06 +0100

    updated k8s provider version

--- terraform/provider/provider.tf
+++ terraform/provider/provider.tf
@@ -13,7 +13,7 @@ terraform {
         }
         kubernetes = {
             source = "hashicorp/kubernetes"
-            version = "~> 2.0.0"
+            version = "3.1.0"
         }
         vcf = {
             source = "vmware/vcf"
