a759dcc3e5468ca8951e21ddf2ccc9dcdfe430fa
Christopher Lewis <christopher-d.lewis@broadcom.com>
2026-05-08 03:08:21 +0100

    updated variables

--- terraform/tenant/main.tf
+++ terraform/tenant/main.tf
@@ -58,6 +58,7 @@ resource "vcfa_supervisor_namespace" "supervisor_namespace" {
   description  = ""
   region_name  = var.namespace_region
   vpc_name     = var.namespace_vpc
+  
 
   storage_classes_initial_class_config_overrides {
     limit = var.namespace_class_storage_class_limit
