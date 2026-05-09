46eebda2200eafb2a2f6280f8589f124cf0cba29
Christopher Lewis <christopher-d.lewis@broadcom.com>
2026-05-09 01:49:39 +0100

    updated

--- terraform/scripts/token.sh
+++ terraform/scripts/token.sh
@@ -11,28 +11,20 @@ if [[ -z "$INPUT" ]]; then
   exit 1
 fi
 
-TOKEN="$(echo "$INPUT" | jq -r '.token')"
-URL="$(echo "$INPUT" | jq -r '.url')"
+TOKEN=$(echo "$INPUT" | jq -r '.token')
+URL=$(echo "$INPUT" | jq -r '.url')
 
 response=$(curl -sk --fail-with-body -X POST \
-  "https://$URL/oauth/provider/token" \
-  -H "Accept: application/json" \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  --data-urlencode "grant_type=refresh_token" \
-  --data-urlencode "refresh_token=$TOKEN")
-
-echo "$response"
+    "https://$URL/oauth/provider/token" \
+    -H "Accept: application/json" \
+    -H "Content-Type: application/x-www-form-urlencoded" \
+    --data-urlencode "grant_type=refresh_token" \
+    --data-urlencode "refresh_token=$TOKEN")
 
 access_token="$(echo "$response" | jq -r '.access_token // empty')"
 
-if [ -z "$access_token" ]; then
-    echo "$response" >&2
-    exit 1
-fi
-
-jq -n \ 
+jq -n \
   --arg access_token "$access_token" \
   '{
     access_token: $access_token
   }'
-  
