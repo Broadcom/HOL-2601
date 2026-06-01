9073a8a4dbaec52a2df4dc400bbb484c721e5dbf
Christopher Lewis <christopher-d.lewis@broadcom.com>
2026-06-01 18:07:45 +0100

    updated tf provider version

--- terraform/tenant/provider.tf
+++ terraform/tenant/provider.tf
@@ -5,7 +5,7 @@ terraform {
     required_providers {
         vcfa = {
             source = "vmware/vcfa"
-            version = "~> 1.0.0"
+            version = "~> 1.1.0"
         }
         vra = {
             source = "vmware/vra"
