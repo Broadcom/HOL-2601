b3a4390ccedde59caabffe7af5bccde71d7ff99c
Christopher Lewis <christopher-d.lewis@broadcom.com>
2026-06-29 09:44:28 +0100

    updates

--- terraform/vcfa-config.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-terraform -chdir=provider init -upgrade
-terraform -chdir=provider apply -auto-approve
-
-# terraform -chdir=tenant init -upgrade
-# terraform -chdir=tenant apply -auto-approve
--- terraform/vcfa-destroy.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-terraform -chdir=tenant destroy -auto-approve
-terraform -chdir=provider destroy -auto-approve
-
